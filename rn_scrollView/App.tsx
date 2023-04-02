25fb226d3c2150ace19ad679f59ed96f4d104b76
duycuongit <118497439+duycuongit@users.noreply.github.com>
2023-04-02 22:02:07 +0700

    learn scroll view

---
+++ rn_scrollView/App.tsx
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import ScrollViewExample from './scroll_view.js';
+
+const App = () => {
+   return (
+      <ScrollViewExample />
+   )
+}
+
+export default App
